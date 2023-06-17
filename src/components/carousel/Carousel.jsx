612056dfa64798f257ad58cd187c34a103cf4c85
utkarsh2543 <utkarshk495@gmail.com>
2023-06-18 01:28:55 +0530

    Home page done

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -10,12 +10,21 @@ import PosterFallback from "../../assets/no-poster.png";
 
 import "./style.scss";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 
 const Carousel = ({ data, loading }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
-  const navigation = (dir) => {};
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+
+    container.scrollTo({ left: scrollAmount, behavior: "smooth" });
+  };
 
   const skItem = () => {
     return (
@@ -35,14 +44,15 @@ const Carousel = ({ data, loading }) => {
         <BsFillArrowLeftCircleFill className="carouselLeftNav arrow" onClick={() => navigation("left")} />
         <BsFillArrowRightCircleFill className="carouselRightNav arrow" onClick={() => navigation("right")} />
         {!loading ? (
-          <div className="carouselItems">
+          <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
               const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback;
               return (
-                <div key={item.id} className="carouselItem">
+                <div key={item.id} className="carouselItem"onClick={()=>navigate(`/${item.media_type}/${item.id}`)} >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />
+                    <Genres data={item.genre_ids.slice(0, 2)} />
                   </div>
                   <div className="textBlock">
                     <span className="title">{item.title || item.name}</span>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import "./style.scss"
-import Herobanner from './heroBanner/Herobanner'
-import Trending from './trending/Trending'
+import React from "react";
+import "./style.scss";
+import Herobanner from "./heroBanner/Herobanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
-    <div className='homepage'>
-      <Herobanner/>
-      <Trending/>
-      <div style={{height:1000}}></div>
+    <div className="homepage">
+      <Herobanner />
+      <Trending />
+      <Popular />
+      <TopRated />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
